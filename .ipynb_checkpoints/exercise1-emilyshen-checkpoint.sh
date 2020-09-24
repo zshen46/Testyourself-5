4bb948302dddd42dfa1005ad5a97b0e48c14c523
EC2 Default User <ec2-user@ip-172-16-10-235.ec2.internal>
2020-09-24 21:53:51 +0000

    Added info about exercise1

---
+++ .ipynb_checkpoints/exercise1-emilyshen-checkpoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Author: Emily Shen
+# The script takes as input one argument, a directory, and outputs the number of different permission types in this directory.
+
+ls -l $1 | cut -d ' ' -f 1 | tail --lines=+2| uniq | wc -l
